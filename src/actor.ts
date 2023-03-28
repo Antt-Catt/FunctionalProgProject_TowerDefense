506fce7d1d3e2aa9a88a76e2a25a924f7f05a722
DURAND Arthur <Arthur.Durand@bordeaux-inp.fr>
2023-03-28 16:38:34 +0200

    Here is the first TOWER (it doesn't work #8)

--- src/actor.ts
+++ src/actor.ts
@@ -1,4 +1,15 @@
+
 type actorType = {
-    pos: pointType
-    actions: {(anActor: actorType, aWorld: worldType):pointType}
+    pos: pointType,
+    type : string,
+    carac: number,
+    actions: {(actor:actorType,aWorld: worldType):pointType}
 }; //i, j position, s speed attack, t attack type
+
+
+const towers :actorType = {
+    pos : 0,
+    type : "tower",
+    carac : 5,
+    actions : function (tower:actorType, world: worldType):
+};
