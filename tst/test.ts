aa789ffa021dc3a93d082f97cc963761cfeff480
HAMOUCHE Luxel <Luxel.Hamouche@bordeaux-inp.fr>
2023-04-04 23:06:13 +0200

    Added functional tests for Point type

--- tst/test.ts
+++ tst/test.ts
@@ -1,7 +1,36 @@
-describe('Main test suite', () => {
+import * as Point from "../src/point.js";
 
-    test('Initial test', () => {
-        expect(1).toBe(1);
+describe('Functional tests for Point', () => {
+
+    test('Point.create', () => {
+        const p: Point.Point = Point.create();
+        const q: Point.Point = Point.create(5, 12);
+        const r: Point.Point = Point.create(1000, -5);
+        expect(p.x).toBe(0);
+        expect(p.y).toBe(0);
+        expect(q.x).toBe(5);
+        expect(q.y).toBe(12);
+        expect(r.x).toBe(1000);
+        expect(r.y).toBe(-5);
+    });
+
+    test('Point.isEqual', () => {
+        const p: Point.Point = Point.create(2, 3);
+        const q: Point.Point = Point.create(2, 3);
+        const r: Point.Point = Point.create(2, -5);
+        expect(Point.isEqual(p, p)).toBe(true);
+        expect(Point.isEqual(p, q)).toBe(true);
+        expect(Point.isEqual(p, r)).toBe(false);
+    });
+
+    test('Point.isInArray', () => {
+        const p: Point.Point = Point.create(2, 3);
+        const q: Point.Point = Point.create(2, 3);
+        const r: Point.Point = Point.create(2, -5);
+        const array: Array<Point.Point> = [p, q];
+        expect(Point.isInArray(p, array)).toBe(true);
+        expect(Point.isInArray(q, array)).toBe(true);
+        expect(Point.isInArray(r, array)).toBe(false);
     });
 
 })
