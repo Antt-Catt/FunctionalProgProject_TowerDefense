cabae734d8504f1f0e3549548975691fe25ba235
Antton Cattarin <antton.cattarin@enseirb-matmeca.fr>
2023-04-04 16:26:18 +0200

    added world display

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import * as Game from "./game.js"
 import * as Point from "./point.js"
+import * as Display from "./display.js"
 
 console.log("[-] main");
 
@@ -8,4 +9,4 @@ const arrayPath: Array<Point.Point> = [{x:0, y:2}, {x:1, y:2}, {x:2, y:2}, {x:3,
 const arrayTower: Array<Point.Point> = [{x:1, y:1}];
 
 const gameState: Game.GameState = Game.init(arrayPath, arrayTower);
-console.log(gameState.world.points);
+Display.displayWorld(gameState.world);
--- src/tile.ts
+++ src/tile.ts
@@ -5,10 +5,6 @@ type Tile = {
     type: 'path' | 'ground' | 'tower';
 };
 
-type PathTile = Tile & {
-    type: 'path';
-}
-
 function create(x: number = 0, y: number = 0, type: 'path' | 'ground' | 'tower' = 'ground'): Tile {
     return { pos: Point.create(x, y), type: type };
 }
