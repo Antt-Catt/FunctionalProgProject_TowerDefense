c1dc9e82f17aa3ed56b7fa40a82ed702b4884eed
Antton Cattarin <antton.cattarin@enseirb-matmeca.fr>
2023-04-06 14:10:14 +0200

    added gameState.actors + initActors (missing actions)

--- src/game.ts
+++ src/game.ts
@@ -1,13 +1,16 @@
 import * as Point from "./point.js";
 import * as World from "./world.js";
+// pour les tests
+import * as Main from "./main.js";
 
 type GameState = {
     world: World.World;
-    // actors: Array<Actors.Actors>;
+    actors: Array<Main.Actor>;
 };
 
 function init(path: Array<Point.Point>, towers: Array<Point.Point>): GameState {
-    return { world: World.init(5, path, towers) };
+    const world = World.init(5, path, towers);
+    return { world: world, actors: Main.initActors(world) };
 }
 
 export {
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
-import * as Game from "./game.js"
-import * as Point from "./point.js"
-import * as Display from "./display.js"
+import * as Game from "./game.js";
+import * as Point from "./point.js";
+import * as Display from "./display.js";
+import * as World from "./world.js";
+import * as Tile from "./tile.js";
+import { log } from "console";
 
 console.log("[-] Game started\n");
 
@@ -12,3 +15,73 @@ const gameState: Game.GameState = Game.init(arrayPath, arrayTower);
 
 console.log("[-] Display the world :");
 console.log(Display.displayWorld(gameState.world));
+console.log(gameState.actors);
+
+///////////////////////////////////////////// est dans actor.ts
+
+type Actor = {
+    position : Point.Point;
+    characteristics ?: Record<string, any>; //pour des infos plus specifiques, par ex les tours : attaque de zone ou unique, ralentir etc.
+    type : "enemy" | "tower";
+}
+
+type Action = (actor: Actor, world: World.World) => number;
+
+type Enemy = Actor & { 
+    type : "enemy";
+    health: number;
+    speed: number;
+    /*actions: {
+        move: Action;
+        // attack: Action;
+    }*/
+    path: Array<Point.Point>;
+}
+
+type Tower = Actor & {
+    type : "tower";
+    damage : number;
+    range : number;
+    cooldown : number;
+    shootable : Array<Tile.Tile>;
+    /*actions: {
+        attack: Action;
+    }*/
+}
+
+//////////////////////////////////////////// à mettre dans actor.ts (ou autre part ?)
+
+function initActors(world: World.World) : Array<Actor> {
+    function initTowers(towers: Array<Point.Point>): Array<Actor> {
+        return towers.map(point => {
+            return { type: "tower",
+            position: point,
+            characteristics : {attack : "unique"},
+            damage : 5,
+            range : 3,
+            cooldown : 1,
+            shootable : [],
+            };
+        });
+    }
+    
+    function initEnemies(n: number, enemies: Array<Enemy>): Array<Actor> {
+        if (n === 0) {
+            return enemies;
+        }
+        return initEnemies(n - 1, enemies.concat({type : "enemy",
+        position : {x:0,y:3},
+        health : 10,
+        speed : 1,
+        path : arrayPath,}));
+    }
+
+    const actors = initTowers(arrayTower).concat(initEnemies(2, []));
+
+    return actors;
+}
+
+export {
+    Actor,
+    initActors,
+};
