81c8b5eb6f6e44a83389b558d2f9cbaef67bdd92
HAMOUCHE Luxel <Luxel.Hamouche@bordeaux-inp.fr>
2023-05-04 10:13:14 +0200

    Added display function for parcel that can print the world on the html page

--- src/display.ts
+++ src/display.ts
@@ -1,12 +1,23 @@
 import * as World from "./world.js";
 
-function displayWorld(world: World.World): string {
-    let result: string = "";
-    world.points.forEach(row => {
-        const rowStr = row.reduce((acc, tile) => { return acc + tile.toString() + ' '; }, '');
-        result += `\n${rowStr}`;
-    });
-    return `${result}\n`;
+function displayWorld(world: World.World) {
+    function displayNode(world: World.World) {
+        let result: string = "";
+        world.points.forEach(row => {
+            result += `\n${row.reduce((acc, tile) => { return acc + tile.toString() + ' '; }, '')}`;
+        });
+        console.log(`${result}\n`);
+    }
+
+    function displayParcel(world: World.World) {
+        world.points.forEach((row, row_index) => {
+            row.forEach((item, item_index) => {
+                (document.getElementsByClassName("case")[row_index*5+item_index] as HTMLElement).style.backgroundColor = (item.type === "path") ? "gray" : "green";
+            }, '');
+        });
+    }
+
+    return (typeof window !== 'undefined') ? displayParcel(world) : displayNode(world);
 }
 
 export {
