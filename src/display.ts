5b016cf35fc7263eee7c3db317c43fe039971818
HAMOUCHE Luxel <Luxel.Hamouche@bordeaux-inp.fr>
2023-05-06 11:46:00 +0200

    Adapted the html table to different size

--- src/display.ts
+++ src/display.ts
@@ -4,7 +4,17 @@ import * as Actor from "./actor.js";
 import * as Tile from "./tile.js";
 
 function initDisplay(gameState: GameState.GameState) {
-    if (typeof window !== 'undefined') {
+    if (typeof window !== "undefined") {
+        const table: HTMLElement = document.querySelector("table") as HTMLElement;
+        for (let i = 0; i < gameState.world.points.length; i++) {
+            const row = document.createElement('tr');
+            for (let j = 0; j < gameState.world.points[0].length; j++) {
+                const cell: HTMLElement = document.createElement('td') as HTMLElement;
+                cell.classList.add('case');
+                row.appendChild(cell);
+            }
+            table.appendChild(row);
+        }
         gameState.world.points.forEach((row, row_index) => {
             row.forEach((item, item_index) => {
                 (document.getElementsByClassName("case")[row_index * 5 + item_index] as HTMLElement).classList.add((item.type === Tile.TileType.Path) ? "path" : "ground");
@@ -32,7 +42,7 @@ function displayWorld(gameState: GameState.GameState) {
             if (Actor.isEnemy(actor) && (actor.position !== Actor.startPosition))
                 index_list.push(Actor.asEnemy(actor).position.y * 5 + Actor.asEnemy(actor).position.x);
         });
-        for (let i = 0; i < gameState.world.points.length*gameState.world.points[0].length; i++) {
+        for (let i = 0; i < gameState.world.points.length * gameState.world.points[0].length; i++) {
             const element: HTMLElement = (document.getElementsByClassName("case")[i] as HTMLElement);
             if ((index_list.includes(i)) !== (element.classList.contains("flower")))
                 element.classList.toggle("flower");
