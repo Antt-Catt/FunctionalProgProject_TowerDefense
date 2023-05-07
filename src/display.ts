ade8f7369e3caa058c9cb9857967bf8798c82ed9
HAMOUCHE Luxel <Luxel.Hamouche@bordeaux-inp.fr>
2023-05-07 10:46:49 +0200

    Refactored display methods

--- src/display.ts
+++ src/display.ts
@@ -5,12 +5,56 @@ import * as Tile from "./tile.js";
 
 const runWithParcel = (): boolean => { return (typeof window !== 'undefined'); };
 
-function initDisplay(gameState: GameState.GameState) {
-    function displayNode(gameStage: GameState.GameState) {
-        console.log(`\n[-] Initial world.\n`);
-    }
+const deepCopy = (obj: any): any => {
+    if (typeof obj !== 'object' || obj === null)
+        return obj;
+    if (Array.isArray(obj))
+        return obj.map(deepCopy);
+    const copy: any = {};
+    Object.keys(obj).forEach((key) => { copy[key] = deepCopy(obj[key]); });
+    return copy;
+};
 
-    function displayParcel(gameStage: GameState.GameState) {
+const copyGameState = (gameState: GameState.GameState): GameState.GameState => {
+    return {
+        world: deepCopy(gameState.world),
+        actors: deepCopy(gameState.actors),
+        path: deepCopy(gameState.path),
+        round: gameState.round,
+        end: gameState.end,
+    };
+};
+
+const gameStateList: Array<GameState.GameState> = [];
+
+/**
+ * Add current game state in game state list.
+ * @param {gameState} GameState.GameState - Current game state.
+ * @returns {void}
+ */
+const update = (gameState: GameState.GameState) => {
+    gameStateList.push(copyGameState(gameState));
+};
+
+/**
+ * Display a message on the correct output stream.
+ * @param {string} message - Message to display.
+ * @returns {void}
+ */
+function displayInfos(message: string) {
+    const displayNode = (message: string) => { console.log(`[-] ${message}`); };
+    const displayParcel = (message: string) => { (document.getElementById("game_text") as HTMLElement).innerHTML = `${message}`; };
+    runWithParcel() ? displayParcel(message) : displayNode(message);
+}
+
+/**
+ * Initialize the display.
+ * @param {GameState.GameState} gameState - Initial game state.
+ * @returns {void}
+ */
+function initDisplay(gameState: GameState.GameState) {
+
+    function displayParcel(gameState: GameState.GameState) {
         const table: HTMLElement = document.querySelector("table") as HTMLElement;
         for (let i = 0; i < gameState.world.points.length; i++) {
             const row = document.createElement('tr');
@@ -32,15 +76,17 @@ function initDisplay(gameState: GameState.GameState) {
         });
     }
 
-    runWithParcel() ? displayParcel(gameState) : displayNode(gameState);
+    gameStateList.push(copyGameState(gameState));
+    displayInfos(`Game Started.`);
+    if (runWithParcel())
+        displayParcel(gameState);
 }
 
-function displayInfos(message: string) {
-    const displayNode = (message: string) => { console.log(`[-] ${message}`); };
-    const displayParcel = (message: string) => { (document.getElementById("game_text") as HTMLElement).innerHTML = `${message}`; };
-    runWithParcel() ? displayParcel(message) : displayNode(message);
-}
-
+/**
+ * Display the world on the correct output stream.
+ * @param {GameState.GameState} gameState - Game state to display.
+ * @returns {void}
+ */
 function displayWorld(gameState: GameState.GameState) {
     function displayNode(world: World.World) {
         let result: string = "";
@@ -54,18 +100,66 @@ function displayWorld(gameState: GameState.GameState) {
         gameState.world.points.forEach((row, row_index) => {
             row.forEach((item, item_index) => {
                 const element: HTMLElement = (document.getElementsByClassName("case")[row_index * 5 + item_index] as HTMLElement);
-                if ((item.type === Tile.TileType.Path && !Tile.isFree(item))  !== (element.classList.contains("flower")))
+                if ((item.type === Tile.TileType.Path && !Tile.isFree(item)) !== (element.classList.contains("flower")))
                     element.classList.toggle("flower");
             }, '');
         });
-        displayNode(gameState.world);
     }
 
     runWithParcel() ? displayParcel(gameState) : displayNode(gameState.world);
 }
 
+/**
+ * Display the end message
+ * @param {GameState.GameState} gameState - The last game state.
+ * @param {number} maxRound - The maximum number of turns.
+ * @returns {void}
+ */
+function displayEnd(gameState: GameState.GameState, maxRound: number) {
+    if (gameState.end)
+        displayInfos(`Game lost, an enemy has reached the end of the course !`);
+    else if (gameState.round > maxRound)
+        displayInfos(`Maximum number of rounds reached !`);
+    else
+        displayInfos(`Win !`);
+}
+
+/**
+ * Display all game states
+ * @param {GameState.GameState} gameState - The last game state.
+ * @param {Array<string>} phases - The name of the phases of the game.
+ * @param {number} maxRound - The maximum number of turns.
+ * @returns {void}
+ */
+function displayAll(gameState: GameState.GameState, phases: Array<string>, maxRound: number) {
+    function displayNode() {
+        gameStateList.forEach((gameState, index) => {
+            if (index !== 0)
+                displayInfos(`Turn ${gameState.round} - Phase ${phases[(index + 1) % phases.length]}.`);
+            displayWorld(gameState);
+        });
+        displayEnd(gameState, maxRound);
+    }
+
+    function displayParcel() {
+        gameStateList.forEach((gameState, index) => {
+            setTimeout(() => {
+                if (index === gameStateList.length - 1)
+                    displayEnd(gameState, maxRound);
+                else {
+                    if (index !== 0)
+                        displayInfos(`Turn ${gameState.round} - Phase ${phases[(index + 1) % phases.length]}.`);
+                    displayWorld(gameState);
+                }
+            }, index * 500);
+        });
+    }
+
+    runWithParcel() ? displayParcel() : displayNode();
+}
+
 export {
     initDisplay,
-    displayInfos,
-    displayWorld,
+    update,
+    displayAll
 };
--- src/motor.ts
+++ src/motor.ts
@@ -13,52 +13,26 @@ let gameState: Game.GameState = Game.init(worldSize, arrayPath, arrayTower);
 
 const phases: Array<Phase.Phase> = Phase.computePhases(gameState);
 
-// Times in ms
-const timePerRound = 2000;
-const timePerPhase = timePerRound / phases.length;
-
-// Print initial world
 Display.initDisplay(gameState);
-Display.displayInfos(`Game started.`);
-Display.displayWorld(gameState);
 
-const interval = setInterval(() => {
-    if (!gameState.end && (gameState.round + 1) <= maxRound) {
-        // Increment and print the round
-        gameState = Game.nextRound(gameState);
+while (!gameState.end && gameState.round < maxRound) {
+    gameState = Game.nextRound(gameState);
 
-        // Move enemies
-        gameState = phases.reduce((game: Game.GameState, aPhase: Phase.Phase) => {
-            const proposals: Array<Point.Point> = game.actors.map((anActor) => {
-                const actor: Actor.Enemy | Actor.Tower = Actor.getActorType(anActor);
-                if (aPhase.name in actor.actions) {
-                    const prop = aPhase.name;
-                    return actor.actions[prop](actor, game.world);
-                }
-                return Actor.startPosition;
-            });
-            const newGame: Game.GameState = Game.resolveProposals(game, proposals, aPhase.resolve, 0);
-
-            // Print world and actors
-            if (!phases.indexOf(aPhase)) {
-                Display.displayInfos(`Turn ${gameState.round} - Phase ${aPhase.name}.`);
-                Display.displayWorld(gameState);
-            } else {
-                setTimeout(() => {
-                    Display.displayInfos(`Turn ${gameState.round} - Phase ${aPhase.name}.`);
-                    Display.displayWorld(gameState);
-                }, timePerPhase * phases.indexOf(aPhase));
+    gameState = phases.reduce((game: Game.GameState, aPhase: Phase.Phase) => {
+        const proposals: Array<Point.Point> = game.actors.map((anActor) => {
+            const actor: Actor.Enemy | Actor.Tower = Actor.getActorType(anActor);
+            if (aPhase.name in actor.actions) {
+                const prop = aPhase.name;
+                return actor.actions[prop](actor, game.world);
             }
+            return Actor.startPosition;
+        });
+        const newGame: Game.GameState = Game.resolveProposals(game, proposals, aPhase.resolve, 0);
 
-            return newGame;
-        }, gameState);
-    } else {
-        clearInterval(interval);
-        if (gameState.end)
-            Display.displayInfos(`Game lost, an enemy has reached the end of the course !`);
-        else if (gameState.round > maxRound)
-            Display.displayInfos(`Maximum number of rounds reached !`);
-        else
-            Display.displayInfos(`Win !`);
-    }
-}, timePerRound);
+        Display.update(gameState);
+
+        return newGame;
+    }, gameState);
+}
+
+Display.displayAll(gameState, phases.map((elt) => elt.name), maxRound);
